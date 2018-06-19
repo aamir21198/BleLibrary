7be4c01cbec2fc27d6875bca3f6e72279e97b8d4
aamir <aamir.darukhanawala@speridian.com>
2018-06-19 10:34:05 +0530

    Add documentation to GattListener

--- app/src/main/java/com/spectrum/ble/GattListener.java
+++ app/src/main/java/com/spectrum/ble/GattListener.java
@@ -6,11 +6,28 @@ import java.util.List;
 
 public interface GattListener {
 
+    /**
+     * Callback when device is connected.
+     */
     public void onDeviceConnected();
 
+    /**
+     * Callback when device is disconnected.
+     */
     public void onDeviceDisconnected();
 
+    /**
+     * Callback when service is discovered.
+     *
+     * @param services List of Gatt services.
+     */
     public void onServiceDiscovered(List<BluetoothGattService> services);
 
+    /**
+     * Callback when data is discovered.
+     *
+     * @param characteristic Characteristic data.
+     * @param data Data in String format.
+     */
     public void onAdvDataDiscovered(byte[] characteristic, String data);
 }
